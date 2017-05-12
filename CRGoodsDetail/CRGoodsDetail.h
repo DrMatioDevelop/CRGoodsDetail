177521065856a2d3ec2558a13c55a3eeb8d01662
d2c_cyf <cyfsoftware@126.com>
2017-05-12 10:18:51 +0800

    Initial Commit

---
+++ CRGoodsDetail/CRGoodsDetail.h
@@ -0,0 +1,19 @@
+//
+//  CRGoodsDetail.h
+//  CRGoodsDetail
+//
+//  Created by d2c_cyf on 17/5/12.
+//  Copyright © 2017年 d2c_cyf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CRGoodsDetail.
+FOUNDATION_EXPORT double CRGoodsDetailVersionNumber;
+
+//! Project version string for CRGoodsDetail.
+FOUNDATION_EXPORT const unsigned char CRGoodsDetailVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CRGoodsDetail/PublicHeader.h>
+
+
